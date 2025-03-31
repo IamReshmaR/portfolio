eb5426e0605c6f8301a3f428bbbdbc8196905ca1
Akhil Ravi <akhilrkn@gmail.com>
2025-03-30 22:30:27 -0500

    Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
+  base: '/portfolio2.0/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/portfolio/',
-}));
+});
